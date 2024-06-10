276540ec07509f7eb2a216df7a50274ba1506b78
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-06-10 15:37:28 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="elabora.php" method="GET">
+    
+    <input type="text" placeholder="Scrivi il tuo testo" name="nome">
+    <input type="text" placeholder="Scrivi il tuo testo" name="cognome">
+    <button type="submit">Invia</button>
+    
+</form>
+
+
+</body>
+</html>
